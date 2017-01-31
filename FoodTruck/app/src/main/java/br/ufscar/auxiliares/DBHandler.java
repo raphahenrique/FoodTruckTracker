70c1ed941e099dc3516602864b4480076de18dca
Yago Arroyo <yagoag@gmail.com>
2017-01-31 19:06:01 -0200

    Adicionado DBHandler

--- FoodTruck/app/src/main/java/br/ufscar/auxiliares/DBHandler.java
+++ FoodTruck/app/src/main/java/br/ufscar/auxiliares/DBHandler.java
@@ -14,8 +14,70 @@ public class DBHandler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String query = "CREATE TABLE foodtruck ();";
+        String query;
+        // Food Trucks
+        query = "CREATE TABLE foodtrucks (" +
+                    "id INTEGER PRIMARY KEY ASC," +
+                    "nome TEXT," +
+                    "priceRange INTEGER," +
+                    "coverPicture BLOB," +
+                    "byOwner INTEGER" +
+                ");";
         sqLiteDatabase.execSQL(query);
+
+        // Food Truck Tags
+        query = "CREATE TABLE tags (" +
+                    "name TEXT," +
+                    "truckId INTEGER," +
+                    "FOREIGN KEY (truckId) REFERENCES foodtrucks (id)" +
+                ");";
+        sqLiteDatabase.execSQL(query);
+
+        // Menu Entries
+        query = "CREATE TABLE menuEntry ("+
+                    "id INTEGER PRIMARY KEY ASC," +
+                    "name TEXT," +
+                    "price REAL," +
+                    "description TEXT," +
+                    "picture BLOB" +
+                    "truckId INTEGER" +
+                    "FOREIGN KEY (truckId) REFERENCES foodtrucks (id)" +
+                ");";
+        sqLiteDatabase.execSQL(query);
+
+        // Menu Entries' Ingredients
+        query = "CREATE TABLE ingredients (" +
+                    "name TEXT," +
+                    "entryId INT," +
+                    "FOREIGN KEY (entryId) REFERENCES menuEntry (id)" +
+                ");";
+        sqLiteDatabase.execSQL(query);
+
+        // Food Truck Locations
+        query = "CREATE TABLE locations (" +
+                    "startDate TEXT," +
+                    "endDate TEXT," +
+                    "latitude TEXT," +
+                    "longitude TEXT," +
+                    "opensAt TEXT," +
+                    "closesBy TEXT," +
+                    "truckId INTEGER," +
+                    "FOREIGN KEY (truckId) REFERENCES foodtrucks (id)" +
+                ");";
+        sqLiteDatabase.execSQL(query);
+
+        // Food Truck Reviews
+        query = "CREATE TABLE reviews (" +
+                    "rating INTEGER," +
+                    "comment TEXT," +
+                    "date TEXT," +
+                    "picture BLOB" +
+                    "truckId INTEGER," +
+                    "FOREIGN KEY (truckId) REFERENCES foodtrucks (id)" +
+                ");";
+        sqLiteDatabase.execSQL(query);
+
+        // TODO: User information
     }
 
     @Override
