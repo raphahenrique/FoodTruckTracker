2801a89319b32a10305a919aa40c3785ae476717
Yago Arroyo <yagoag@gmail.com>
2017-02-13 10:48:39 -0200

    Added Data class to store all the data for now

---
+++ FoodTruck/app/src/main/java/br/ufscar/auxiliares/Data.java
@@ -0,0 +1,36 @@
+package br.ufscar.auxiliares;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import br.ufscar.foodtruck.Truck;
+
+public class Data {
+    private static int truckId = 0;
+    public static List<Truck> truckList = new LinkedList<>();
+
+    public static void addTruck(Truck t) {
+        if (t.getId() == -1) {
+            t.setId(truckId);
+            truckId++;
+            truckList.add(t);
+        } else {
+            for (int i = 0; i < truckList.size(); i++) {
+                if (t.getId() == truckList.get(i).getId()) {
+                    truckList.remove(i);
+                    break;
+                }
+
+            }
+            truckList.add(t);
+        }
+    }
+
+    public static Truck getTruckById(int id) {
+        for (Truck t : truckList) {
+            if (t.getId() == id)
+                return t;
+        }
+        return null;
+    }
+}
